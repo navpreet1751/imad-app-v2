59a725745700b5bb7404627eba51401fec7905bd
navpreet1751 <navhappy92@gmail.com>
2017-02-12 20:58:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ var articles = {
         <p>
             This is the content of my third article.
         </p>
-        <<p>
+        <p>
             This is the content of my third article.
         </p>`},
     'article-four': {
@@ -44,7 +44,7 @@ var articles = {
         <p>
             This is the content of my fourth article.
         </p>
-        <<p>
+        <p>
             This is the content of my fourth article.
         </p>`},
 };
