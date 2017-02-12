cfe157875a44b7538878bac4e91ac8ee2379a629
navpreet1751 <navhappy92@gmail.com>
2017-02-13 00:19:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ function moveRight(){
     marginLeft = marginLeft + 1;
     img.style.marginLeft= marginLeft + 'px';
 }
-img.onclick = function(){
+imgage.onclick = function(){
     var interval = setInterval(moveRight,50);
    
 };
