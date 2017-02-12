0d8bed52eb902c2def426e939e65d436db79d1ee
navpreet1751 <navhappy92@gmail.com>
2017-02-12 18:04:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
@@ -19,14 +20,14 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/article-one', function (req, res) {
-  res.send("Article one requested and will be served here ");
+app.get('/profile', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
+});
+app.get('/article-1', function (req, res) {
+  res.send("Article 1 requested and will be served here ");
 });
 app.get('/article-2', function (req, res) {
-  res.send("Article Two requested and will be served here ");
-});
-app.get('/article-3', function (req, res) {
-  res.send("Article Three requested and will be served here ");
+  res.send("Article 2 requested and will be served here ");
 });
 
 
