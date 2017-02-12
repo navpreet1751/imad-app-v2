afb33cb1d124fabd639cde84d001ad07c9c18881
navpreet1751 <navhappy92@gmail.com>
2017-02-13 00:22:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,10 @@ var image = document.getElementById('singh');
 var marginLeft = 0;
 
 function moveRight(){
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     image.style.marginLeft= marginLeft + 'px';
 }
 image.onclick = function(){
-    var interval = setInterval(moveRight,50);
+    var interval = setInterval(moveRight,1);
    
 };
